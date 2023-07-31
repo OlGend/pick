af07e19b515b5c093ce9bbc811f4596b2f864070
Oleh <gend4016@gmail.com>
2023-07-31 18:21:20 +0300

    add files seconds change

--- app/guides/[id]/page.tsx
+++ app/guides/[id]/page.tsx
@@ -1,22 +1,16 @@
 import { Metadata } from "next";
 import jsonContent from "@/components/guidePosts.json";
 import Image from "next/image";
-import Img from "@/public/how-to-beat-wagering-requirements.jpg";
 
 type Props = {
   params: {
     id: string;
   };
 };
+
 interface ContentItem {
   tag: string;
   fullContent?: string;
-  divContent?: {
-    [key: string]: {
-      tag: string;
-      fullContent: string;
-    };
-  };
 }
 
 interface GuidePost {
@@ -24,12 +18,8 @@ interface GuidePost {
   title: string;
   excerpt: string;
   image: string;
-  content: {
-    [key: string]: ContentItem;
-  };
+  content: ContentItem[];
 }
-
-
 
 export async function generateMetadata({
   params: { id },
@@ -62,30 +52,24 @@ export default function GuidePost({ params: { id } }: Props) {
           <div className="avatar mr-3"></div>
           <div className="flex flex-col">
             <div>Gamblinator editorial</div>
-            <div className="date">
-              Updated on July 31, 2023
-            </div>
+            <div className="date">Updated on July 31, 2023</div>
           </div>
         </div>
-        <Image className="pt-4" src={`/${post.image}`} alt={post.title} width={600} height={300} loading="lazy" />
+        <Image
+          className="pt-4"
+          src={`/${post.image}`}
+          alt={post.title}
+          width={600}
+          height={300}
+          loading="lazy"
+        />
         <div>
           {post.content &&
-            Object.values(post.content).map((item, index) => {
+            post.content.map((item, index) => {
               if (item.tag === "p") {
-                return <p key={index}>{item.fullContent}</p>;
+                return <p key={index}>{item.fullContent || ""}</p>;
               } else if (item.tag === "h3") {
-                return <h3 key={index}>{item.fullContent}</h3>;
-              } else if (item.tag === "div" && item.divContent) {
-                return (
-                  <div className="example-block" key={index}>
-                    {Object.values(item.divContent).map((subItem, subIndex) => (
-                      <div  key={subIndex}>
-                        {subItem.tag === "h3" && <h3>{subItem.fullContent}</h3>}
-                        {subItem.tag === "p" && <p>{subItem.fullContent}</p>}
-                      </div>
-                    ))}
-                  </div>
-                );
+                return <h3 key={index}>{item.fullContent || ""}</h3>;
               } else {
                 return null;
               }
