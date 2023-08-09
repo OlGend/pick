e8d4d78cae23c81aad8f70dfe7531051adf235a0
Oleh <gend4016@gmail.com>
2023-08-09 19:15:51 +0300

    add files seconds change442dsw

--- components/BrandSearchPage.jsx
+++ components/BrandSearchPage.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
-import SearchBrands from './SearchBrands';
-import SearchResult from './SearchResult';
-import { useTopBrands } from './useBrands';
+// import React, { useState } from 'react';
+// import { useRouter } from 'next/router';
+// import SearchBrands from './SearchBrands';
+// import SearchResult from './SearchResult';
+// import { useTopBrands } from './useBrands';
 
-export default function BrandSearchPage() {
-  const [searchResults, setSearchResults] = useState([]);
-  const router = useRouter();
+// export default function BrandSearchPage() {
+//   const [searchResults, setSearchResults] = useState([]);
+//   const router = useRouter();
 
-  const handleSearch = async (query) => {
-    const filteredBrands = await useTopBrands(query); // Используйте вашу функцию для фильтрации брендов
-    setSearchResults(filteredBrands);
-  };
+//   const handleSearch = async (query) => {
+//     const filteredBrands = await useTopBrands(query); // Используйте вашу функцию для фильтрации брендов
+//     setSearchResults(filteredBrands);
+//   };
 
-  const handleBrandClick = (brandId) => {
-    router.push(`/bonuses/${brandId}`);
-  };
+//   const handleBrandClick = (brandId) => {
+//     router.push(`/bonuses/${brandId}`);
+//   };
 
-  return (
-    <div>
-      <h1>Brand Search Page</h1>
-      <SearchBrands onSearch={handleSearch} />
-      {searchResults.length > 0 && <SearchResult brands={searchResults} />}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <h1>Brand Search Page</h1>
+//       <SearchBrands onSearch={handleSearch} />
+//       {searchResults.length > 0 && <SearchResult brands={searchResults} />}
+//     </div>
+//   );
+// }
