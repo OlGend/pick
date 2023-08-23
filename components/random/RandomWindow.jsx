e4dda50c599e902214b277d72be368b3a590d523
Oleh <gend4016@gmail.com>
2023-08-23 17:38:06 +0300

    commit 23.08.01

--- components/random/RandomWindow.jsx
+++ components/random/RandomWindow.jsx
@@ -13,39 +13,46 @@ import {
 const Modal = () => {
   const [showModal, setShowModal] = useState(false);
   const [randomBrand, setRandomBrand] = useState(null);
+  const [isActive, setIsActive] = useState(false); // Добавляем состояние для класса "active"
   const brandData = useTopBrands(112);
 
   useEffect(() => {
-    const lastClosedTime = localStorage.getItem("lastClosedTime");
+    const timer = setTimeout(() => {
+      const lastClosedTime = localStorage.getItem("lastClosedTime");
 
-    if (!lastClosedTime || Date.now() - lastClosedTime > 600000) {
-      const randomIndex = Math.floor(Math.random() * brandData.length);
-      setRandomBrand(brandData[randomIndex]);
-      setShowModal(true);
-    }
+      if (!lastClosedTime || Date.now() - lastClosedTime > 60000) {
+        const randomIndex = Math.floor(Math.random() * brandData.length);
+        setRandomBrand(brandData[randomIndex]);
+        setShowModal(true);
+        setIsActive(true); // Добавляем класс "active" через 5 секунд
+      }
+    }, 5000); // 5000 миллисекунд = 5 секунд
+
+    return () => clearTimeout(timer);
   }, [brandData]);
 
   const closeModal = () => {
     setShowModal(false);
+    setIsActive(false); // Удаляем класс "active"
     localStorage.setItem("lastClosedTime", Date.now());
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
       const lastClosedTime = localStorage.getItem("lastClosedTime");
-      if (!lastClosedTime || Date.now() - lastClosedTime > 600000) {
+      if (!lastClosedTime || Date.now() - lastClosedTime > 60000) {
         const randomIndex = Math.floor(Math.random() * brandData.length);
         setRandomBrand(brandData[randomIndex]);
         setShowModal(true);
       }
-    }, 600000); // 600000 миллисекунд = 1 минут
+    }, 60000); // 1000 миллисекунд = 10 минут
 
     return () => clearInterval(interval);
   }, [brandData]);
 
   return (
     showModal && randomBrand && (
-      <div className="modal-overlay">
+      <div className={`modal-overlay ${isActive ? "active" : ""}`}>
         <div className="modal-content relative">
           <p>Random Brand</p>
           <span className="mb-2">Pay attention to a random brand suggested just for you</span>
