91f521b41250dd983b3775ab28d1a47feba2881c
Oleh <gend4016@gmail.com>
2023-09-12 16:16:58 +0300

    title for brand

--- app/bonuses/[id]/page.tsx
+++ app/bonuses/[id]/page.tsx
@@ -4,6 +4,13 @@ import CarouselSlider from "@/components/CarouselSlider";
 import NewBrands from "@/components/NewBrands";
 import GuideSlotsPage from "@/components/GuideSlotsPage";
 
+async function getData(id: string) {
+  // Здесь выполняйте загрузку данных бренда, например, с использованием fetch
+  const response = await fetch(`https://hotoffers.casino/wp-json/wp/v2/affiliates/${id}`);
+  const data = await response.json();
+  return data;
+}
+
 type Props = {
   params: {
     id: string;
@@ -13,16 +20,19 @@ type Props = {
 export async function generateMetadata({
   params: { id },
 }: Props): Promise<Metadata> {
+  const brand = await getData(id);
   return {
-    title: "Bonuses | XXLCasinoList | ",
+    title: `${brand.title.rendered} | XXLCasinoList | Bonuses`,
   };
 }
 
+
 export default async function Brand({ params: { id } }: Props) {
+  const brand = await getData(id);
   return (
     <>
       <div className="main__container brandUtils">
-        <BrandById brand={id} />
+        <BrandById brand={brand.id} />
         <CarouselSlider slides={[]} />
         <div className="wrapper-container background-block">
           <NewBrands />
