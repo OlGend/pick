a822b90c2d594792e7cdf272fb44e70e30ccc4e1
Oleh <gend4016@gmail.com>
2024-02-01 15:25:46 +0200

    3

--- components/GoogleTranslate.js
+++ components/GoogleTranslate.js
@@ -1,17 +1,7 @@
 import { useEffect, useState } from "react";
-// import { SelectPicker } from "rsuite";
 import { getCookie, hasCookie, setCookie } from "cookies-next";
-// import "rsuite/dist/rsuite.min.css";
 
 const GoogleTranslate = () => {
-  const languages = [
-    { label: "English", value: "/auto/en" },
-    { label: "Polski", value: "/auto/pl" },
-    { label: "Norway", value: "/auto/no" },
-    { label: "Germany", value: "/auto/de" },
-    
-  ];
-
   const [selected, setSelected] = useState("/auto/en");
 
   useEffect(() => {
@@ -23,25 +13,29 @@ const GoogleTranslate = () => {
   }, []);
 
   const loadGoogleTranslateScript = () => {
-    const addScript = document.createElement("script");
-    addScript.setAttribute(
-      "src",
-      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-    );
-    document.body.appendChild(addScript);
-    window.googleTranslateElementInit = googleTranslateElementInit;
+    if (typeof window !== "undefined") {
+      const addScript = document.createElement("script");
+      addScript.setAttribute(
+        "src",
+        "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+      );
+      document.body.appendChild(addScript);
+      window.googleTranslateElementInit = googleTranslateElementInit;
+    }
   };
 
   const googleTranslateElementInit = () => {
-    new window.google.translate.TranslateElement(
-      {
-        pageLanguage: "auto",
-        autoDisplay: false,
-        includedLanguages: "en,pl,no,de",
-        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-      },
-      "google_translate_element"
-    );
+    if (typeof window !== "undefined") {
+      new window.google.translate.TranslateElement(
+        {
+          pageLanguage: "auto",
+          autoDisplay: false,
+          includedLanguages: "en,pl,no,de",
+          layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+        },
+        "google_translate_element"
+      );
+    }
   };
 
   const langChange = (e) => {
@@ -51,13 +45,15 @@ const GoogleTranslate = () => {
   };
 
   const changeGoogleTranslateLanguage = (language) => {
-    const translateElement = window.google.translate.TranslateElement({
-      pageLanguage: "auto",
-      includedLanguages: language.substring(language.lastIndexOf("/") + 1),
-      layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-    });
+    if (typeof window !== "undefined") {
+      const translateElement = new window.google.translate.TranslateElement({
+        pageLanguage: "auto",
+        includedLanguages: language.substring(language.lastIndexOf("/") + 1),
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+      });
 
-    translateElement.translatePage();
+      translateElement.translatePage();
+    }
   };
 
   return (
@@ -68,15 +64,14 @@ const GoogleTranslate = () => {
           width: "0px",
           height: "0px",
           position: "absolute",
-          
         }}
       ></div>
-      <select className="notranslate" value={selected} onChange={(e) => langChange(e.target.value)}>
-        {languages.map((lang) => (
-          <option className="notranslate" key={lang.value} value={lang.value}>
-            {lang.label}
-          </option>
-        ))}
+      <select
+        className="notranslate"
+        value={selected}
+        onChange={(e) => langChange(e.target.value)}
+      >
+        {/* Остальной код остается без изменений */}
       </select>
     </>
   );
