4fa3481261b7cc936aa5d5839895dc24cc5f7765
Oleh <gend4016@gmail.com>
2024-02-16 16:05:37 +0200

    slider

--- components/AllBrands.jsx
+++ components/AllBrands.jsx
@@ -36,7 +36,7 @@ import {
 
 export default function AllBrands({ choose, filtered, isLoader }) {
   const { t } = useTranslation();
-  const itemsPerPage = 4;
+  const itemsPerPage = 6;
   const [visibleBrands, setVisibleBrands] = useState(itemsPerPage);
   const [hasMoreBrands, setHasMoreBrands] = useState(false);
   const [openPlusesId, setOpenPlusesId] = useState(null);
@@ -108,19 +108,23 @@ export default function AllBrands({ choose, filtered, isLoader }) {
   const [randomBrands, setRandomBrands] = useState([]);
   const [randomBrands2, setRandomBrands2] = useState([]);
 
+  const [brandsGenerated, setBrandsGenerated] = useState(false);
 
   useEffect(() => {
-    const shuffledBrands = topBrands.sort(() => Math.random() - 0.5);
-    const shuffledBrands2 = filteredBrands.sort(() => Math.random() - 0.5);
+    if (!brandsGenerated) {
+      const shuffledBrands = topBrands.sort(() => Math.random() - 0.5);
+      const shuffledBrands2 = filteredBrands.sort(() => Math.random() - 0.5);
 
-    const selectedBrands = shuffledBrands.slice(0, 4);
-    const selectedBrands2 = shuffledBrands2.slice(0, 4);
+      const selectedBrands = shuffledBrands.slice(0, 4);
+      const selectedBrands2 = shuffledBrands2.slice(0, 4);
 
-    setRandomBrands(selectedBrands);
-    setRandomBrands2(selectedBrands2);
+      setRandomBrands(selectedBrands);
+      setRandomBrands2(selectedBrands2);
+      setBrandsGenerated(true);
+    }
+  }, [brandsGenerated, filteredBrands, topBrands]);
 
-  }, []);
-
+  console.log("RANDOMBRANDS@", randomBrands2);
   return (
     <>
       {isLoader ? (
@@ -128,150 +132,145 @@ export default function AllBrands({ choose, filtered, isLoader }) {
       ) : (
         <div className="flex flex-wrap justify-between">
           <div className="flex flex-col px-0 py-6 basis-[75%]">
-          {randomBrands2.map((brand) => {
-              const reviewImgSrc = extractReviewImage(brand.content.rendered);
-              const playLink = extractLink(brand.content.rendered);
-              const isPlusesOpen = openPlusesId === brand.id;
-              const isWithdrawalOpen = openWithdrawalId === brand.id;
-              const isDepositsOpen = openDepositsId === brand.id;
-              const isCountriesOpen = openCountriesId === brand.id;
+            {filteredBrands
+              .slice(0, visibleBrands)
+              .sort(() => Math.random() - 0.5)
+              .map((brand) => {
+                const reviewImgSrc = extractReviewImage(brand.content.rendered);
+                const playLink = extractLink(brand.content.rendered);
+                const isPlusesOpen = openPlusesId === brand.id;
+                const isWithdrawalOpen = openWithdrawalId === brand.id;
+                const isDepositsOpen = openDepositsId === brand.id;
+                const isCountriesOpen = openCountriesId === brand.id;
 
-              return (
-                <div
-                  className="p-3 flex justify-between flex-wrap mb-2 card-brand-filtered w-full"
-                  key={brand.id}
-                >
-                  <div className="flex flex-col basis-[63%]">
-                    <div className="flex ml-1 mb-3">
-                      {/* <div className="filter-flag">{filtered.flag}</div> */}
-                    </div>
-                    <div className="flex mb-1">
-                      <Gift className="mr-1" size={24} />
+                return (
+                  <div
+                    className="p-3 flex justify-between flex-wrap mb-2 card-brand-filtered w-full"
+                    key={brand.id}
+                  >
+                    <div className="flex flex-col basis-[63%]">
+                      <div className="flex ml-1 mb-3">
+                        {/* <div className="filter-flag">{filtered.flag}</div> */}
+                      </div>
+                      <div className="flex mb-1">
+                        <Gift className="mr-1" size={24} />
+                        <div
+                          className=" flex items-center"
+                          dangerouslySetInnerHTML={{
+                            __html: extractReviewBonus(brand.content.rendered),
+                          }}
+                        />
+                      </div>
+                      <div className="mb-2 withdrawal withdrawal-limits flex items-center">
+                        <Handshake className="mr-1 mb-1" size={24} />
+                        <div className="title mr-2">Withdrawal Limits:</div>
+                        <div
+                          className="items-center"
+                          dangerouslySetInnerHTML={{
+                            __html: extractLimits(brand.content.rendered),
+                          }}
+                        />
+                      </div>
                       <div
-                        className=" flex items-center"
-                        dangerouslySetInnerHTML={{
-                          __html: extractReviewBonus(brand.content.rendered),
-                        }}
-                      />
-                    </div>
-                    <div className="mb-2 withdrawal withdrawal-limits flex items-center">
-                      <Handshake className="mr-1 mb-1" size={24} />
-                      <div className="title mr-2">
-                      Withdrawal Limits:
-                      </div>
-                      <div
-                        className="items-center"
-                        dangerouslySetInnerHTML={{
-                          __html: extractLimits(brand.content.rendered),
-                        }}
-                      />
-                    </div>
-                    <div
-                      onClick={() => handlePlusesClick(brand.id)}
-                      className="pluses custom-list-item mb-1"
-                    >
-                      <div className="title flex items-center">
-                        <ShieldPlus size={24} />
-                        <span className="mt-1 ml-2">
-                        Advantages
-                        </span>
-                        <CaretDown className="ml-auto" size={20} />
-                      </div>
-                      {isPlusesOpen && (
-                        <div
-                          className="items-center ml-3"
-                          dangerouslySetInnerHTML={{
-                            __html: extractPros(brand.content.rendered),
-                          }}
-                        />
-                      )}
-                    </div>
-                    <div
-                      onClick={() => handleDepositsClick(brand.id)}
-                      className="withdrawal custom-list-item mb-1"
-                    >
-                      <div className="title flex items-center">
-                        <CurrencyCircleDollar size={24} />
-                        <span className="mt-1 ml-2">
-                        Deposit Methods
-                        </span>
-                        <CaretDown className="ml-auto" size={20} />
-                      </div>
-                      {isDepositsOpen && (
-                        <div
-                          className="withdrawal"
-                          dangerouslySetInnerHTML={{
-                            __html: extractDeposits(brand.content.rendered),
-                          }}
-                        />
-                      )}
-                    </div>
-                    <div
-                      onClick={() => handleWithdrawalClick(brand.id)}
-                      className="withdrawal custom-list-item mb-1"
-                    >
-                      <div className="title flex items-center">
-                        <Bank size={24} />
-                        <span className="mt-1 ml-2">
-                        Withdrawal Methods
-                        </span>
-                        <CaretDown className="ml-auto" size={20} />
-                      </div>
-                      {isWithdrawalOpen && (
-                        <div
-                          className="withdrawal"
-                          dangerouslySetInnerHTML={{
-                            __html: extractWithdrawal(brand.content.rendered),
-                          }}
-                        />
-                      )}
-                    </div>
-                    <div
-                      onClick={() => handleCountriesClick(brand.id)}
-                      className="withdrawal custom-list-item mb-1"
-                    >
-                      <div className="title flex items-center">
-                        <Prohibit size={24} />
-                        <span className="mt-1 ml-2">
-                        Restricted Countries
-                        </span>
-                        <CaretDown className="ml-auto" size={20} />
-                      </div>
-                      {isCountriesOpen && (
-                        <div className="withdrawal">
-                          {/* Виводимо обмежені країни */}
-                          <div className="countries flex flex-wrap justify-between mt-1">
-                            {extractCountries(brand.content.rendered).map(
-                              (country, index) => (
-                                <div
-                                  className="basis-[49%] pl-1 mb-2 flex"
-                                  key={index}
-                                >
-                                  <MinusCircle color="#dd3333" size={18} />
-                                  <span className="pl-1">{country}</span>
-                                </div>
-                              )
-                            )}
-                          </div>
+                        onClick={() => handlePlusesClick(brand.id)}
+                        className="pluses custom-list-item mb-1"
+                      >
+                        <div className="title flex items-center">
+                          <ShieldPlus size={24} />
+                          <span className="mt-1 ml-2">Advantages</span>
+                          <CaretDown className="ml-auto" size={20} />
                         </div>
-                      )}
+                        {isPlusesOpen && (
+                          <div
+                            className="items-center ml-3"
+                            dangerouslySetInnerHTML={{
+                              __html: extractPros(brand.content.rendered),
+                            }}
+                          />
+                        )}
+                      </div>
+                      <div
+                        onClick={() => handleDepositsClick(brand.id)}
+                        className="withdrawal custom-list-item mb-1"
+                      >
+                        <div className="title flex items-center">
+                          <CurrencyCircleDollar size={24} />
+                          <span className="mt-1 ml-2">Deposit Methods</span>
+                          <CaretDown className="ml-auto" size={20} />
+                        </div>
+                        {isDepositsOpen && (
+                          <div
+                            className="withdrawal"
+                            dangerouslySetInnerHTML={{
+                              __html: extractDeposits(brand.content.rendered),
+                            }}
+                          />
+                        )}
+                      </div>
+                      <div
+                        onClick={() => handleWithdrawalClick(brand.id)}
+                        className="withdrawal custom-list-item mb-1"
+                      >
+                        <div className="title flex items-center">
+                          <Bank size={24} />
+                          <span className="mt-1 ml-2">Withdrawal Methods</span>
+                          <CaretDown className="ml-auto" size={20} />
+                        </div>
+                        {isWithdrawalOpen && (
+                          <div
+                            className="withdrawal"
+                            dangerouslySetInnerHTML={{
+                              __html: extractWithdrawal(brand.content.rendered),
+                            }}
+                          />
+                        )}
+                      </div>
+                      <div
+                        onClick={() => handleCountriesClick(brand.id)}
+                        className="withdrawal custom-list-item mb-1"
+                      >
+                        <div className="title flex items-center">
+                          <Prohibit size={24} />
+                          <span className="mt-1 ml-2">
+                            Restricted Countries
+                          </span>
+                          <CaretDown className="ml-auto" size={20} />
+                        </div>
+                        {isCountriesOpen && (
+                          <div className="withdrawal">
+                            {/* Виводимо обмежені країни */}
+                            <div className="countries flex flex-wrap justify-between mt-1">
+                              {extractCountries(brand.content.rendered).map(
+                                (country, index) => (
+                                  <div
+                                    className="basis-[49%] pl-1 mb-2 flex"
+                                    key={index}
+                                  >
+                                    <MinusCircle color="#dd3333" size={18} />
+                                    <span className="pl-1">{country}</span>
+                                  </div>
+                                )
+                              )}
+                            </div>
+                          </div>
+                        )}
+                      </div>
                     </div>
-                  </div>
-                  <div className="basis-[36%]">
-                    <div className="brandImage p-3">
-                      <Link key={brand.id} href={`/bonuses/${brand.id}`}>
-                        <Image
-                          src={reviewImgSrc}
-                          alt={brand.title.rendered}
-                          width={250}
-                          height={125}
-                          loading="lazy"
-                        />
-                      </Link>
-                    </div>
+                    <div className="basis-[36%]">
+                      <div className="brandImage p-3">
+                        <Link key={brand.id} href={`/bonuses/${brand.id}`}>
+                          <Image
+                            src={reviewImgSrc}
+                            alt={brand.title.rendered}
+                            width={250}
+                            height={125}
+                            loading="lazy"
+                          />
+                        </Link>
+                      </div>
 
-                    <div className="buttons ml-auto flex items-center">
-                      {/* <Link
+                      <div className="buttons ml-auto flex items-center">
+                        {/* <Link
                         className="btn btn-secondary text-center flex justify-center items-center"
                         href={`/bonuses/${brand.id}`}
                         onClick={handleLinkClick}
@@ -283,26 +282,24 @@ export default function AllBrands({ choose, filtered, isLoader }) {
                         )}
                       Read Review
                       </Link> */}
-                      <div className="flex flex-col items-center w-full p-4 howUse mt-2 mb-2">
-                        <span className="text-center">
-                        How to get bonus?
-                        </span>
-                        <p className="text-center m-0 text-slate-500">
-                        Activate bonus in your casino account
-                        </p>
+                        <div className="flex flex-col items-center w-full p-4 howUse mt-2 mb-2">
+                          <span className="text-center">How to get bonus?</span>
+                          <p className="text-center m-0 text-slate-500">
+                            Activate bonus in your casino account
+                          </p>
+                        </div>
+                        <Link
+                          className="btn btn-primary mt-0 text-center flex justify-center items-center"
+                          href={`https://link.reg2dep1.com/${playLink}/${newUrl}`}
+                          target="_blank"
+                        >
+                          <Play className="mr-2" size={24} /> Play Now
+                        </Link>
                       </div>
-                      <Link
-                        className="btn btn-primary mt-0 text-center flex justify-center items-center"
-                        href={`https://link.reg2dep1.com/${playLink}/${newUrl}`}
-                        target="_blank"
-                      >
-                        <Play className="mr-2" size={24} /> Play Now
-                      </Link>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
 
             {hasMoreBrands && (
               <button
@@ -315,7 +312,10 @@ export default function AllBrands({ choose, filtered, isLoader }) {
             )}
           </div>
           <div className="flex flex-col basis-[24%] py-6">
-            {randomBrands.map((item) => {
+          {topBrands
+              .slice(0, visibleBrands)
+              .sort(() => Math.random() - 0.5)
+              .map((item) => {
               const reviewImgSrc = extractReviewImage(item.content.rendered);
               const playLink = extractLink(item.content.rendered);
               return (
@@ -351,6 +351,3 @@ export default function AllBrands({ choose, filtered, isLoader }) {
     </>
   );
 }
-
-
-  
